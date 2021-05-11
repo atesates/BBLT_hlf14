973f5f0cd6fcb02f1758e29a58c41cdc7a148fae
atesates <atesates2012@gmail.com>
2021-05-10 23:46:00 -0700

    constant_pacing

--- javascript/test/create/channel_population/create/locustfile.py
+++ javascript/test/create/channel_population/create/locustfile.py
@@ -6,6 +6,7 @@ from locust import HttpUser, task, between
 class WebsiteUser(HttpUser):
     # wait between 2-3 seconds
     wait_time = between(0.1, 0.3)
+    #wait_time = constant_pacing(1)
     
 
     @task
